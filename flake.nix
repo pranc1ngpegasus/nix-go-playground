302ee19745f1e0c9003810127edc9139305168a7
pranc1ngpegasus <temma.fukaya@mokmok.dev>
2024-09-19 22:18:14 +0900

    feat: setup nix flake

---
+++ flake.nix
@@ -0,0 +1,66 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+    pre-commit-hooks.url = "github:cachix/git-hooks.nix";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    pre-commit-hooks,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = nixpkgs.legacyPackages.${system};
+        checks = pre-commit-hooks.lib.${system}.run {
+          src = ./.;
+          hooks = {
+            alejandra = {
+              enable = true;
+              entry = "alejandra .";
+              files = "^.*\\.nix$";
+              language = "system";
+              pass_filenames = false;
+            };
+            golangci-lint = {
+              enable = true;
+              entry = "golangci-lint run ./...";
+              files = "^.*\\.go$";
+              language = "system";
+              pass_filenames = false;
+            };
+            gotest = {
+              enable = true;
+              entry = "go test ./...";
+              files = "^.*_test\\.go$";
+              language = "system";
+              pass_filenames = false;
+            };
+            govet = {
+              enable = true;
+              entry = "go vet ./...";
+              files = "^.*\\.go$";
+              language = "system";
+              pass_filenames = false;
+            };
+          };
+        };
+      in {
+        devShells.default = pkgs.stdenv.mkDerivation {
+          name = "nix-go-playground";
+          nativeBuildInputs = [];
+          buildInputs = with pkgs; [
+            go_1_23
+            golangci-lint
+            httpie
+          ];
+          shellHook = ''
+            ${checks.shellHook}
+          '';
+        };
+      }
+    );
+}
